566f0f0e64e13f57dec47abe5fa7779470995da6
Qadeer Afzal <Qadeerdev12@gmail.com>
2026-05-17 01:45:46 -0400

    Weather App - Get weather by Latitude and longitude

--- weatherReport/weather_report.js
+++ weatherReport/weather_report.js
@@ -1,8 +1,9 @@
 function showweatherDetails(event) {
     event.preventDefault();
-    const city = document.getElementById('city').value;
+    const lon = document.getElementById('lon').value;
+    const lat = document.getElementById('lat').value;
     const apiKey = 'c4f86ece00bc8aa272652ac9065af12d'; // Replace 'YOUR_API_KEY' with your actual API key
-    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
 
     fetch(apiUrl)
     .then(response => response.json())
